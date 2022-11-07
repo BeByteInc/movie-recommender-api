dc1cc31e3e970730f342c6eb1af84dca3bf6443f
Bytewaiser <bytewaiser@gmail.com>
2022-11-08 00:04:54 +0300

    Auth Bug Fixed

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from models.result import Result
-from models.auth import AuthDetails
+from models.user import AuthDetails
 from services.movie_service import MovieService
 from services.auth_service import AuthHandler
 from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import pandas as pd
 import uvicorn
 
@@ -11,23 +10,25 @@ app = FastAPI()
 auth_handler = AuthHandler()
 
 user_database_path = "resources/datasets/logindata.parquet"
-user_database = pd.read_parquet(user_database_path)
 
 @app.post("/register", status_code=201)
 def register(auth_details: AuthDetails):
+    user_database = pd.read_parquet(user_database_path)
     if (user_database.username == auth_details.username).sum() > 0:
         raise HTTPException(status_code=400, detail='Username is taken')
     hashed_password = auth_handler.get_password_hash(auth_details.password)
-    temp = user_database.append({
+    user_database.append({
         "id": user_database.id.max() + 1,
         "username": auth_details.username,
-        "password": hashed_password
-    }, ignore_index=True)
-    temp.to_parquet(user_database_path, index=False)
-    return {"Username": auth_details.username}
+        "password": hashed_password,
+        "verification_code": auth_details.verification_code
+    }, ignore_index=True).to_parquet(user_database_path, index=False)
+    token = auth_handler.encode_token(auth_details.username)
+    return {"token": token}
 
 @app.post("/login")
 def login(auth_details: AuthDetails):
+    user_database = pd.read_parquet(user_database_path)
     data = user_database[user_database.username == auth_details.username].to_dict(orient="records")
     print(data)
     user = None if len(data) == 0 else data[0]
@@ -35,7 +36,6 @@ def login(auth_details: AuthDetails):
         raise HTTPException(status_code=401, detail="Invalid username and/or password")
     token = auth_handler.encode_token(user["username"])
     return { "token": token }
-
 
 movie_service = MovieService()
 @app.get("/get_movie_by_id", response_model=Result)
@@ -59,4 +59,4 @@ async def search_by_title_with_genre(search_key: str, genre_name: str, _=Depends
     return Result.build(type="SimpleMovieList", function=movie_service.search_by_title_with_genre_name, search_key=search_key, genre_name=genre_name)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", reload=True)
+    uvicorn.run("app:app", reload=True, host="0.0.0.0")
--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
 class AuthDetails(BaseModel):
+    email: str
     username: str
     password: str
+    verification_code: int
