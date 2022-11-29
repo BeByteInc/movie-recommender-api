69fce923724117d16413d617125a27d006d635df
Bytewaiser <bytewaiser@gmail.com>
2022-11-29 23:45:48 +0300

    Changed path variables

--- path.py
+++ path.py
@@ -1,6 +1,1 @@
 database = "resources/datasets/recommendation.db"
-movie_data_path = "resources/datasets/movies.parquet"
-recommendation_data_path = "resources/datasets/favorites.parquet"
-user_database_path = "resources/datasets/logindata.csv"
-
-
