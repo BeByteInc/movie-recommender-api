b173f30dd79f50ad47a6cc7d81c84794bfbae33c
Bytewaiser <bytewaiser@gmail.com>
2022-12-01 01:41:37 +0300

    Root path created

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ import uvicorn
 
 from services.recommendation_service import RecommendationService
 
-app = FastAPI()
+root_path = "/movierec"
+app = FastAPI(root_path=root_path)
 auth_handler = AuthHandler()
 
 movie_service = MovieService()
 recommendation_service = RecommendationService()
 
-@app.post("/register", status_code=201)
+@app.post(f"{root_path}/register", status_code=201)
 def register(auth_details: RegisterDetail):
     user = recommendation_service.get_user(auth_details.username)
     if user is not None:
@@ -27,7 +28,7 @@ def register(auth_details: RegisterDetail):
     response = {"username": auth_details.username, "user_id": user_id, "token": token, "item_list": []}
     return response
 
-@app.post("/login")
+@app.post(f"{root_path}/login")
 def login(auth_details: LoginDetail):
     user = recommendation_service.get_user(auth_details.username)
 
@@ -41,45 +42,46 @@ def login(auth_details: LoginDetail):
     response = {"username": auth_details.username, "user_id": user[0], "token": token, "item_list": recommendation_service.get_user_favorites(user[0])}
     return response
 
-@app.get("/get_movie_by_id", response_model=Result)
+@app.get(f"/{root_path}/get_movie_by_id", response_model=Result)
 async def get_movie_by_id(id: int, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="Movie", function=movie_service.get_movie_by_id, id=id)
 
-@app.get("/get_top_rated_movies", response_model=Result)
+@app.get(f"/{root_path}/get_top_rated_movies", response_model=Result)
 async def get_top_rated_movies(page:int, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=movie_service.get_top_rated_movies, page=page)
 
-@app.get("/get_top_rated_movies_by_genre_name", response_model=Result)
+@app.get(f"/{root_path}/get_top_rated_movies_by_genre_name", response_model=Result)
 async def get_top_rated_movies_by_genre_name(page: int, genre_name: str, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=movie_service.get_top_rated_movies_by_genre_name, page=page, genre_name=genre_name)
 
-@app.get("/search_by_title_all_data", response_model=Result)
+@app.get(f"/{root_path}/search_by_title_all_data", response_model=Result)
 async def search_by_title_all_data(search_key: str, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=movie_service.search_by_title_all_data, search_key=search_key)
 
-@app.get("/search_by_title_with_genre_name", response_model=Result)
+@app.get(f"/{root_path}/search_by_title_with_genre_name", response_model=Result)
 async def search_by_title_with_genre(search_key: str, genre_name: str, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=movie_service.search_by_title_with_genre_name, search_key=search_key, genre_name=genre_name)
 
-@app.get("/recommend", response_model=Result)
+@app.get(f"/{root_path}/recommend", response_model=Result)
 async def recommend(user_id: int, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=recommendation_service.recommend, user_id=user_id)
 
-@app.post("/recommend_by_genre_names", response_model=Result)
+@app.post(f"/{root_path}/recommend_by_genre_names", response_model=Result)
 async def recommend_by_genre(user: RecommendByGenreModel, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=recommendation_service.recommend_with_genre, user_id=user.user_id, genre_names=user.genre_names)
 
-@app.post("/set_user_favorites")
+@app.post(f"/{root_path}/set_user_favorites")
 async def set_user_favorites(user: UserFavorites, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=recommendation_service.set_user_favorites, user_id=user.user_id, movie_ids=user.movie_ids)
 
-@app.post("/add_to_user_favorites")
+@app.post(f"/{root_path}/add_to_user_favorites")
 async def add_user_favorites(user: UserFavorite,_=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=recommendation_service.add_user_favorites, user_id=user.user_id, movie_id=user.movie_id)
 
-@app.post("/remove_from_user_favorites")
+@app.post(f"/{root_path}/remove_from_user_favorites")
 async def remove_user_favorites(user: UserFavorite, _=Depends(auth_handler.auth_wrapper)):
     return Result.build(type="SimpleMovieList", function=recommendation_service.remove_user_favorites, user_id=user.user_id, movie_id=user.movie_id)
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", reload=True, host="0.0.0.0")
